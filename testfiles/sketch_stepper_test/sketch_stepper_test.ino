f8aaad1b2ab588568046b7f5de6c269c24bfa7bc
Marc <pansmarcjoe@t-online.de>
2022-01-13 17:45:21 +0100

    added wemos d1 compatibilty

--- testfiles/sketch_stepper_test/sketch_stepper_test.ino
+++ testfiles/sketch_stepper_test/sketch_stepper_test.ino
@@ -1,15 +1,15 @@
 #include <AccelStepper.h>
-#define dirPin 2
-#define stepPin 3
+#define dirPin 14    // D5
+#define stepPin 12   // D6
+#define enablepin 13 // D7
 #define motorInterfaceType 1
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
-
 void setup()
 {
     stepper.setMaxSpeed(100000);
     stepper.setSpeed(10000);
     stepper.setAcceleration(20000);
-    stepper.moveTo(-5000);
+    stepper.moveTo(-500000000);
 }
 
 void loop()
